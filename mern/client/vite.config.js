6ca46d0acdd80f4396043bc80bb3f3c9875ef350
Lexie Zhang <zay20040811@gmail.com>
2024-05-04 21:00:46 -0600

    made sure default changed from dist to build

--- mern/client/vite.config.js
+++ mern/client/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build',  // This line sets the build output directory to 'build'
+  }
 })
