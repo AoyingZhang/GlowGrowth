69ea7e2d46099b539c02de74e22a1bad2b914bd0
Lexie Zhang <zay20040811@gmail.com>
2024-05-14 00:52:37 -0400

    added useNavigate

--- mern/client/src/components/Signup.jsx
+++ mern/client/src/components/Signup.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { TextField, Button, Typography, Container, Box, Paper, Alert } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { Link , useNavigate} from 'react-router-dom';
 import { API_BASE_URL } from '../config';
 
 function Signup() {
